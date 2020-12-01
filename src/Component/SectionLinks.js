02a1ec8ec6fa5dcc96d4e1dfc45930bbe1ba20d2
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-12-01 16:44:29 -0500

    navbar

--- src/Component/SectionLinks.js
+++ src/Component/SectionLinks.js
@@ -1,16 +1,27 @@
 import React from 'react'
 
+
+
+function addResponse() {
+    let x = document.querySelector(".topnav");
+    x.classList.toggle("responsive")
+  }
+
+
 function SectionLinks () {
     return (
         <div id="SectionLinks"> 
 
-            <nav>
-                <a href="">Home</a>
-                <a href="">Bio</a>
-                <a href="">Projects</a>
-                <a href="">Experiences</a>
-                <a href="">Education</a>
-            </nav>
+            <div class="topnav">
+                <a href="#" class="active">Home</a>
+                <a href="#">Bio</a>
+                <a href="#">Projects</a>
+                <a href="#">Experiences</a>
+                <a href="#">Education</a>
+                <a href="javascript:void(0);" class="icon" onClick={addResponse}>
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
 
         </div>
     )
