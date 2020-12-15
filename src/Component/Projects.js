68bc6f3707967b4644624255719c79e31a0a0131
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-12-15 16:22:27 -0500

    projects

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -17,6 +17,10 @@ function Projects (){
 
             <div className="slides project5">
                 <h4>UnderCover Restaurant Inspector App</h4>
+                <div class="projectlinks">
+                    <a href="https://mod5restaurant-app-e8887.firebaseapp.com/" target="_blank">Live</a>
+                    <a href="https://youtu.be/XsgI-8kNW9s" target="_blank">Demo</a>
+                </div>
                 <Carousel>
                     <Carousel.Item>
                         <img
@@ -76,9 +80,14 @@ function Projects (){
                     </Carousel.Item>
                 </Carousel>
             </div>
+            <p className="plinebreak"> </p>
 
             <div className="slides project4">
                 <h4>My Waifu and Husbando Dating App</h4>
+                <div class="projectlinks">
+                    <a href="https://mod4dating-app.firebaseapp.com/" target="_blank">Live</a>
+                    <a href="https://youtu.be/cNt6Rvme8z8" target="_blank">Demo</a>
+                </div>
                 <Carousel>
                     <Carousel.Item>
                         <img
@@ -138,9 +147,188 @@ function Projects (){
                     </Carousel.Item>
                 </Carousel>
             </div>
+            <p className="plinebreak"> </p>
+
+            <div className="slides project3">
+                <h4>Ice Cream Bar App</h4>
+                <div class="projectlinks">
+                    <a href="https://mod3icecream-app.firebaseapp.com/" target="_blank">Live</a>
+                    <a href="https://youtu.be/pN6BFduQLqY" target="_blank">Demo</a>
+                </div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607981927/ProfileProject/p3-01_lc7cai.png"
+                        alt="01 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607981931/ProfileProject/p3-02_ubvumb.png"
+                        alt="02 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607981931/ProfileProject/p3-03_jfo4us.png"
+                        alt="03 slide"
+                         />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <p className="plinebreak"> </p>
+
+            <div className="slides project2">
+                <h4>Cozy Nation App</h4>
+                <div class="projectlinks">
+                    <a href="https://mod2-pets.herokuapp.com/" target="_blank">Live</a>
+                    <a href="https://youtu.be/4TgWvjdpRgM" target="_blank">Demo</a>
+                </div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983603/ProfileProject/p2-01_za1cw8.png"
+                        alt="01 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983604/ProfileProject/p2-02_yursrq.png"
+                        alt="02 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983599/ProfileProject/p2-03_nao6bw.png"
+                        alt="03 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983598/ProfileProject/p2-04_btwic0.png"
+                        alt="04 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983599/ProfileProject/p2-05_gdikve.png"
+                        alt="05 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983598/ProfileProject/p2-06_ccifny.png"
+                        alt="06 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983602/ProfileProject/p2-07_advgqh.png"
+                        alt="07 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983604/ProfileProject/p2-08_jbi7f1.png"
+                        alt="08 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607983601/ProfileProject/p2-09_iut9qb.png"
+                        alt="09 slide"
+                         />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <p className="plinebreak"> </p>
+
+            <div className="slides project1">
+                <h4>Movie Mate App</h4>
+                <div class="projectlinks">
+                    <a href="https://youtu.be/POL5bgMKg4g" target="_blank">Demo</a>
+                </div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984467/ProfileProject/p1-01_zuzpho.png"
+                        alt="01 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984464/ProfileProject/p1-02_xaektr.png"
+                        alt="02 slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984465/ProfileProject/p1-03_cmjgq5.png"
+                        alt="03 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984464/ProfileProject/p1-04_t1jqig.png"
+                        alt="04 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984466/ProfileProject/p1-05_ybyeoc.png"
+                        alt="05 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984463/ProfileProject/p1-06_msiwkj.png"
+                        alt="06 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984465/ProfileProject/p1-07_bldygy.png"
+                        alt="07 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984466/ProfileProject/p1-08_co1tdc.png"
+                        alt="08 slide"
+                         />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://res.cloudinary.com/dfqzcsl8x/image/upload/v1607984467/ProfileProject/p1-09_qqwbdx.png"
+                        alt="09 slide"
+                         />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <p className="plinebreak"> </p>
 
 
-  
         </div>
         </>
     )
